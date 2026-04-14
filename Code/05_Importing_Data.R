4c1a98a3a3c8c16f6b60eb07359e1ff720917a16
ArcangeliNicola <nicola.arcangeli3@studio.unibo.it>
2026-04-14 16:55:25 +0200

    Update 05_Importing_Data.R

--- Code/05_Importing_Data.R
+++ Code/05_Importing_Data.R
@@ -1,1 +1,56 @@
+#code to import data
+
+library(terra)
+library(viridis)
+library(imageRy)
+
+setwd("C:/Scienze e gestione della natura/Primo anno/Telerilevamento")
+getwd()
+
+#list of files 
+list.files()
+
+gre<-rast("DJI_20260409152942_0001_MS_G.tiff")
+#flippiamo
+gre<-flip(gre)
+#plot
+plot(gre, col=mako(100))
+
+red<-flip(rast("DJI_20260409152942_0001_MS_R.tiff"))
+nir<-flip(rast("DJI_20260409152942_0001_MS_NIR.tiff"))
+
+#stack
+stack<-c(gre, red, nir)
+plot(stack)
+#ricordarsi lo stretch
+plotRGB(stack, r=3,g=2,b=1, stretch="lin")
+#stretch per valori intermedi 
+plotRGB(stack, r=3,g=2,b=1, stretch="hist")
+
+im.multiframe(1,2)
+plotRGB(stack, r=3,g=2,b=1, stretch="lin")
+plotRGB(stack, r=3,g=2,b=1, stretch="hist")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
