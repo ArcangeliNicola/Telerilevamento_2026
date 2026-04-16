06b31a68edab236381b00a111cf8f77d143950f7
ArcangeliNicola <nicola.arcangeli3@studio.unibo.it>
2026-04-16 15:34:37 +0200

    Update 06_Exam_example.R

--- Code/06_Exam_example.R
+++ Code/06_Exam_example.R
@@ -1,15 +1,22 @@
 library(terra)
+library(imageRy)
+setwd("C:/Scienze e gestione della natura/Primo anno/Telerilevamento")
+getwd()
 
+list.files()
 
+richat<-flip(rast("richatstructure_oli_20260306.jpg"))
+plot(richat)
 
+png("figura.png")
+plot(richat)
+dev.off()
 
-
-
-
-
-
-
-
+png("ric b1 e b2.png")
+im.multiframe(2,1)
+plot(richat[[1]])
+plot(richat[[2]])
+dev.off()
 
 
 
