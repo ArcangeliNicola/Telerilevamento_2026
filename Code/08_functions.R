b738ae44b7fb59c9b61b471304d6c97b37705d4a
ArcangeliNicola <nicola.arcangeli3@studio.unibo.it>
2026-04-30 14:55:45 +0200

    Update 08_functions.R

--- Code/08_functions.R
+++ Code/08_functions.R
@@ -10,18 +10,38 @@ differenza<-function(x,y){
   return(z)
   }
 
+#par
+mf <- function(nrow,ncol) {
+  par(mfrow=c(nrow,ncol))
+  }
+#struttura if/else
 
+positivo<-function(x) {
+if(x>0) {
+  "questo numero è positivo"}
+else if(x<0) {
+"questo numero è negativo"}
+else {
+"Zero non è ne negativo ne positivo"}
+}
 
+#ciclo for
 
+loop <-function(){
+for(i in 1:50)
+  print(i)
+  }
 
+loop2 <-function(){
+for(i in 1:50)
+  {op<-(i*7)
+  print(op)
+  }
+}
 
-
-
-
-
-
-
-
+setwd("C:\\Scienze e gestione della natura\\Primo anno\\Telerilevamento")
+sink("output.txt") #funzione per salvare la roba bisogna mettere la roba che si vuole salvare dopo
+loop2()   #funzione che viene sincata
 
 
 
